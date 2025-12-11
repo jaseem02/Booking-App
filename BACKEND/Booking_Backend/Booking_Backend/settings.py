8088125d26fa320cf87a1ce195a51e7860c2e7ab
jaseem02 <muhammadjaseem2051@gmail.com>
2025-12-11 23:01:08 +0530

    Fix duplicate staticfiles error and update deployment settings

--- BACKEND/Booking_Backend/Booking_Backend/settings.py
+++ BACKEND/Booking_Backend/Booking_Backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
