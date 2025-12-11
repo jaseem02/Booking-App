11c02163da494a9111628ba4b99556ceddbdadfa
jaseem02 <muhammadjaseem2051@gmail.com>
2025-12-11 21:47:01 +0530

    Initial commit

---
+++ BACKEND/Booking_Backend/Room_Booking/serializers.py
@@ -0,0 +1,39 @@
+
+from rest_framework import serializers
+
+from .models import Room,RoomImage,OccupiedDate,User
+
+
+class RoomImageSerializer(serializers.ModelSerializer):
+    room = serializers.HyperlinkedRelatedField(
+        view_name = 'room-detail',
+        queryset= Room.objects.all()),
+    class Meta:
+        model = RoomImage
+        fields = ['id','image','caption','room']
+        
+        
+class RoomSerializer(serializers.HyperlinkedModelSerializer):
+    images = RoomImageSerializer(many=True,read_only=True)
+    class Meta:
+        model = Room
+        fields = ['url','id','name','type','pricePerNight','currency','maxOccupancy','description','images']
+        
+class OccupiedDateSerializer(serializers.HyperlinkedModelSerializer):
+    room = serializers.HyperlinkedRelatedField(
+        view_name = 'room-detail',
+        queryset = Room.objects.all()
+    )
+    class Meta:
+        model = OccupiedDate
+        fields = ['url','id','room','date']
+        
+        
+from django.contrib.auth.hashers import make_password
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url','id','username','password','email','full_name']
+        
+    def validate_password(self,value):
+        return make_password(value)
