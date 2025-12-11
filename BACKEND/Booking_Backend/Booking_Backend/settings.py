11289c292b131eef696c068ade3f335b5e2dde97
jaseem02 <muhammadjaseem2051@gmail.com>
2025-12-11 23:33:29 +0530

    Fix middleware list formatting (prevent concatenated middleware strings)

--- BACKEND/Booking_Backend/Booking_Backend/settings.py
+++ BACKEND/Booking_Backend/Booking_Backend/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.common.CommonMiddleware'
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
